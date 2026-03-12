a158e39dcdfe24cd96b0dabbfd015e8c121e06e7
vishal_kumar_888 <jonhalexe000@gmail.com>
2026-03-12 17:15:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (14.37%)

---
+++ 0069-sqrtx/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low =0, high = x;
+        while(low<=high){
+            long long mid = (low+high)/2;
+            if((mid*mid) <= x) low = mid+1;
+            else high = mid-1;
+        }
+        return high;
+    }
+};
