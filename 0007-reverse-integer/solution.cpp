03e1a1269a936ab32ab315c63a25e124e06186e1
vishal_kumar_888 <jonhalexe000@gmail.com>
2026-03-15 04:02:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (54.39%)

---
+++ 0007-reverse-integer/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long reversed = 0;
+        while (x != 0) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+        }
+        if (reversed > INT_MAX || reversed < INT_MIN)
+            return 0;
+        return (int)reversed;
+    }
+};
