deb20e1ab25e145d21b968a0e6638cda8a489389
vishal_kumar_888 <jonhalexe000@gmail.com>
2026-05-06 09:33:06 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 71.8 MB (52.71%)

---
+++ 0053-maximum-subarray/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+       /*  int maxi = INT_MIN;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int sum = 0;
+            for (int j = i; j < nums.size(); j++) {
+                sum += nums[j];
+                maxi = max(sum ,maxi);
+            }
+            
+        }
+        return maxi; */
+
+        int maxi = INT_MIN;
+        int sum =0; 
+        for(int i: nums){
+            sum += i;
+            maxi = max(maxi,sum);
+            if(sum < 0) sum =0;
+        }
+        return maxi;
+    }
+};
