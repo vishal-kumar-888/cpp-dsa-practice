86df66d6cc9901131e9712974e2380a7a6578f81
vishal_kumar_888 <jonhalexe000@gmail.com>
2026-07-08 02:03:39 +0000

    Sync LeetCode submission Runtime - 3 ms (28.08%), Memory - 95.9 MB (25.54%)

---
+++ 0004-median-of-two-sorted-arrays/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    vector<int> arr;
+
+    int i = 0, j = 0;
+
+    while (i < nums1.size() && j < nums2.size()) {
+        if (nums1[i] < nums2[j])
+            arr.push_back(nums1[i++]);
+        else
+            arr.push_back(nums2[j++]);
+    }
+
+    while (i < nums1.size())
+        arr.push_back(nums1[i++]);
+
+    while (j < nums2.size())
+        arr.push_back(nums2[j++]);
+
+    int n = arr.size();
+
+    if (n % 2 == 1)
+        return arr[n / 2];
+
+    return (arr[n / 2 - 1] + arr[n / 2]) / 2.0;
+}
+};
