9857d2ecac9d7fb4520a3cf31f3a4dad43f6348f
vishal_kumar_888 <jonhalexe000@gmail.com>
2026-08-08 08:58:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 22.7 MB (52.91%)

--- 0026-remove-duplicates-from-sorted-array/solution.cpp
+++ 0026-remove-duplicates-from-sorted-array/solution.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        // If array is empty, return 0 directly
-        if (nums.empty()) return 0;
+        int slow =0;
 
-        // Pointer for the position of last unique element
-        int i = 0;
-
-        // Traverse the array starting from the second element
-        for (int j = 1; j < nums.size(); j++) {
-            // If current element is different from last unique element
-            if (nums[j] != nums[i]) {
-                // Move pointer for unique element forward
-                i++;
-                // Place the new unique element at the next position
-                nums[i] = nums[j];
+        for(int fast=1;fast<nums.size();fast++){
+            if(nums[slow]!= nums[fast]){
+                slow++;
+                nums[slow] = nums[fast];
+                
             }
         }
-
-        // i is index of last unique element, count = i + 1
-        return i + 1;
+        return slow + 1;
     }
 };
