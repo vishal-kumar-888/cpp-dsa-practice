4cbf2362ccfc97b09f7382da5a453d360a55243b
vishal_kumar_888 <jonhalexe000@gmail.com>
2026-08-20 04:32:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.9 MB (25.86%)

--- 0242-valid-anagram/solution.cpp
+++ 0242-valid-anagram/solution.cpp
@@ -1,9 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s == t) return true;
-        return false;
+         if (s.length() != t.length()) return false;
+    
+    unordered_map<char, int> freq;
+    
+    // Count characters in s
+    for (char c : s) {
+        freq[c]++;
+    }
+    
+    // Subtract characters in t
+    for (char c : t) {
+        freq[c]--;
+        if (freq[c] < 0) {
+            return false;
+        }
+    }
+    
+    return true;
     }
 };
