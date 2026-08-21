6d7b50a5354759d9f27af8a34019299ef1873f7c
vishal_kumar_888 <jonhalexe000@gmail.com>
2026-08-21 03:37:06 +0000

    Sync LeetCode submission Runtime - 11 ms (9.49%), Memory - 44.7 MB (53.46%)

--- 0713-subarray-product-less-than-k/solution.cpp
+++ 0713-subarray-product-less-than-k/solution.cpp
@@ -1,36 +1,17 @@
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-    //     int n = nums.size(), ans = 0;
-
-    //     for(int i = 0; i < n; i++) {
-    //         int product = 1;
-
-    //         for(int j = i; j < n; j++) {
-    //             product *= nums[j];
-
-    //             if(product < k)
-    //                 ans++;
-    //             else
-    //                 break;
-    //         }
-    //     }
-
-    //     return ans;
-    // }
-
-     if(k <= 1) return 0;
-
-    int left =0, right =0,ans =0;
-    double product =1;
-    for(right =0;right<nums.size();right++){
-        product*=nums[right];
-        while(product>=k){
-            product/=nums[left];
-            left++;
+        if (k <= 1) return 0;
+        int left =0,right =0, ans =0;
+        long long product = 1;
+        for(right =0;right<nums.size();right++){
+            product *=nums[right];
+            while(product>= k){
+                product = product/nums[left];
+                left++;
+            }
+            ans += (right-left+1);
         }
-        ans += (right-left+1);
-    }
-    return ans;
+        return ans;
     }
 };
