de33e22d7a3d1898efbec61d254110230367d3fb
vishal_kumar_888 <jonhalexe000@gmail.com>
2026-09-07 05:36:12 +0000

    Sync LeetCode submission Runtime - 12 ms (7.32%), Memory - 13.2 MB (12.62%)

---
+++ 0142-linked-list-cycle-ii/solution.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* detectCycle(ListNode* head) {
+        unordered_set<ListNode*> visited;
+        ListNode* temp = head;
+        while (temp != NULL) {
+            if (visited.find(temp) != visited.end()) {
+                return temp;
+            }
+            visited.insert(temp);
+            temp = temp->next;
+        }
+        return nullptr;
+    }
+
+};
