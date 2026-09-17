5c667d299d32779be5ff0805fdf4bb28f0b430a6
vishal_kumar_888 <jonhalexe000@gmail.com>
2026-09-17 15:43:04 +0000

    Sync LeetCode submission Runtime - 18 ms (50.09%), Memory - 64.7 MB (15.36%)

---
+++ 0969-number-of-recent-calls/solution.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+
+class RecentCounter {
+private:
+    std::vector<int> records;
+    int start;
+
+public:
+    RecentCounter() : start(0) {}
+
+    int ping(int t) {
+        records.push_back(t);
+        while (records[start] < t - 3000) {
+            start++;
+        }
+        return records.size() - start;
+    }
+};
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
