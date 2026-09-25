e3aa45143dc1e82433f46a581176fa6d5fc044b4
vishal_kumar_888 <jonhalexe000@gmail.com>
2026-09-25 13:00:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.8 MB (96.01%)

---
+++ 0078-subsets/solution.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void ab(vector<vector<int>>& ans, vector<int>& nums,
+            vector<int>& cu, int i) {
+
+        if (i == nums.size()) {
+            ans.push_back(cu);
+            return;
+        }
+
+        // TAKE
+        cu.push_back(nums[i]);
+        ab(ans, nums, cu, i + 1);
+
+        // UNDO
+        cu.pop_back();
+
+        // SKIP
+        ab(ans, nums, cu, i + 1);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> cu;
+
+        ab(ans, nums, cu, 0);
+
+        return ans;
+    }
+};
